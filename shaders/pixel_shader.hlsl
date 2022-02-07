d0a1ec45f6a471301548663379b0f86bc1cf2851
MarcusS64 <marcus@skolds.com>
2022-02-07 11:45:41 +0100

    Fixed shaders
    fixed vector calculations

--- shaders/pixel_shader.hlsl
+++ shaders/pixel_shader.hlsl
@@ -32,13 +32,13 @@ cbuffer PhongShinyBuffer : register(b1)
 
 float4 PS_main(PSIn input) : SV_Target
 {
-	float3 LightVector1 = normalize(LightPosition1 - input.WorldPos);
-	float3 ReflectVector1 = normalize(reflect(LightVector1, input.Normal));
+	float3 LightVector1 = normalize(LightPosition1.xyz - input.WorldPos);
+	float3 ReflectVector1 = normalize(reflect(LightVector1.xyz, input.Normal));
 
 	/*float3 LightVector2 = normalize(LightPosition2 - input.worldPos);
 	float3 ReflectVector2 = normalize(reflect(LightVector2, input.Normal));*/
 
-	float3 ViewVector = normalize(CamPosition - input.TexCoord);
+	float3 ViewVector = normalize(CamPosition.xyz - input.WorldPos);
 
 	/*float3 LightSum = (mul(Diffuse.xyz, LightVector1 * input.Normal) + mul(Specular.xyz, pow(ReflectVector1 * ViewVector, 1)))
 		+ (mul(Diffuse.xyz, LightVector2 * input.Normal) + mul(Specular.xyz, pow(ReflectVector2 * ViewVector, 1)));*/
@@ -46,7 +46,7 @@ float4 PS_main(PSIn input) : SV_Target
 	float Shinyness = Specular.w;
 
 	float3 A = Ambient.xyz;
-	float3 D = mul(Diffuse.xyz, max(dot(LightVector1, input.Normal), 0)); 
+	float3 D = mul(Diffuse.xyz, max(dot(LightVector1.xyz, input.Normal), 0));
 	float3 S = mul(Specular.xyz, pow(max(dot(ReflectVector1, ViewVector), 0), Shinyness));
 	//float3 DiffuseColor = mul(DiffuseTexColor.xyz, dot(LightVector1, input.Normal));
 	// Debug shading #1: map and return normal as a color, i.e. from [-1,1]->[0,1] per component
--- shaders/vertex_shader.hlsl
+++ shaders/vertex_shader.hlsl
@@ -15,7 +15,7 @@ struct VSIn
 	float3 Tangent : TANGENT;
 	float3 Binormal : BINORMAL;
 	float2 TexCoord : TEX;
-	float3 WorldPos : Pos;
+	
 };
 
 struct PSIn
@@ -45,6 +45,6 @@ PSIn VS_main(VSIn input)
 	output.Pos = mul(MVP, float4(input.Pos, 1));
 	output.Normal = normalize( mul(ModelToWorldMatrix, float4(input.Normal,0)).xyz );
 	output.TexCoord = input.TexCoord;
-	output.WorldPos = normalize(mul(ModelToWorldMatrix, float4(input.WorldPos, 1)).xyz);
+	output.WorldPos = mul(ModelToWorldMatrix, float4(input.Pos, 1)).xyz;
 	return output;
 }
